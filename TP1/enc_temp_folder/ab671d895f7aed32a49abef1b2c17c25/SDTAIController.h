bc0f2bb3da187bef71fa517f87de46108d4f8f32
Sikap <49215307+Sikap@users.noreply.github.com>
2023-02-01 17:26:02 -0500

    exposition des paramètres clapmin

--- TP1/enc_temp_folder/ab671d895f7aed32a49abef1b2c17c25/SDTAIController.h
+++ TP1/enc_temp_folder/ab671d895f7aed32a49abef1b2c17c25/SDTAIController.h
@@ -29,22 +29,22 @@ public:
     virtual void Tick(float deltaTime) override; 
 
     // Max speed of the Agent
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI, meta = (ClampMin = 0))
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI)
     float m_max_speed = 300;
     // Acceleration of the Agent
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI, meta = (ClampMin = 0))
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI)
     float m_acceleration = 425;
     // Agent detection reduis of Collectables 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI, meta = (ClampMin = 0))
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI)
     float m_Collectebale_detection_reduis = 500;
     // Buffer space between Agent and wall
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI, meta = (ClampMin = 0))
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI)
      float m_wall_cast = 200;
     // Buffer space between Agent and death wall
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI, meta = (ClampMin = 0))
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI)
      float m_side_cast = 100;
     // Feild vision of Agent
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI, meta = (ClampMin = 0))
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI)
      float m_vision_angle = PI / 2.00f;
 
 private:
