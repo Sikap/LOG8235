53670bc439dc0e94ba75c733d808bc0f9ef1af34
Antonin Boucarut <antonin.boucarut@rexys.io>
2023-04-14 22:24:34 -0400

    link BT with controller and character, also changes the characters logic in the scene

--- TP3/Source/SoftDesignTraining/SoftDesignTrainingCharacter.h
+++ TP3/Source/SoftDesignTraining/SoftDesignTrainingCharacter.h
@@ -1,6 +1,8 @@
 // Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
 #pragma once
 #include "GameFramework/Character.h"
+#include "BehaviorTree/BehaviorTree.h"
+
 #include "SoftDesignTrainingCharacter.generated.h"
 
 
@@ -15,8 +17,12 @@ public:
     virtual void BeginPlay() override;
     virtual void OnCollectPowerUp() {};
     void Die();
-
+    UBehaviorTree* GetBehaviorTree() const { return m_aiBehaviorTree; }
 protected:
+
+    UPROPERTY(EditAnywhere, category = Behavior)
+        UBehaviorTree* m_aiBehaviorTree;
+
     UFUNCTION()
     virtual void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
